30b4fa57cbb716269e439024bb8c0383dc6977fb
policeman18 <usasergio2@gmail.com>
2021-09-18 13:03:55 +0200

    Primera versión

---
+++ rndNumber.js
@@ -0,0 +1,27 @@
+//Numero aleatorio 1-10
+let rnd = Math.floor(Math.random() * 10)+1;
+
+//Variables
+let acertado = false;
+let continuar = undefined;
+let intentos = 0;
+
+while(!acertado){
+    console.log("[DEBUG] rnd number: " +rnd);
+
+    let input = prompt("Elija un número aleatorio entre el 1-100");
+
+    //Si el usuario acierta
+    if(input==rnd) { 
+        alert("Has acertado el numero! \nHas hecho un total de "+intentos + " intentos");
+        acertado=true;
+    }
+    else {
+        if(input >= rnd)
+            alert("Has fallado! El número es menor o igual.");
+        else
+            alert("Has fallado! El número es mayor o igual.");
+        acertado = false;
+        intentos++;
+    }
+}
